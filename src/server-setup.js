8f0cb96bb28c1013cc1bbbb90e4877c11aa9d4da
Thiago Paiva <thiago.rodriguesdepaiva@gmail.com>
2023-04-07 17:29:52 -0300

    feat: adicionando cors para liberar conexões de acesso na api

--- src/server-setup.js
+++ src/server-setup.js
@@ -1,10 +1,12 @@
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
+import cors from "@koa/cors";
 
 import { router } from "./routes";
 
 const app = new Koa();
 
+app.use(cors());
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
